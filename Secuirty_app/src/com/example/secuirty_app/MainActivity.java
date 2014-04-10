23c863cda37859d5fabc4c9371266febd701288b
ayee5 <alvinec09@yahoo.com>
2014-04-09 21:01:35 -0700

    Restore fragment layout. Set login page as default layout. Import google play service library.

--- Secuirty_app/src/com/example/secuirty_app/MainActivity.java
+++ Secuirty_app/src/com/example/secuirty_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.secuirty_app;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
